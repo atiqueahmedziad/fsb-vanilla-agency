1fad280a9252e0935441c08dee977d2b9ff506d7
Atique Ahmed Ziad <softfilebd@gmail.com>
2020-12-29 18:16:46 +0600

    Update fsb-vanilla-apps.js

--- fsb-vanilla-apps.js
+++ fsb-vanilla-apps.js
@@ -40,70 +40,115 @@ function updateProgressBar(goalAmount, cartAmount) {
 }
 
 function setupProgressBar(progressBarWidth) {
-  $('body').prepend(`<div class="fsb-progress-bar"></div>`);
+  const progBarEle = document.createElement('div');
+  progBarEle.classList.add('fsb-progress-bar');
 
-  $('.fsb-progress-bar').css({
-    "opacity": "1",
-    "margin": "0px",
-    "padding": "0px",
-    "left": "0px",
-    "width": `${progressBarWidth}%`,
-    "z-index": "1000000001",
-    "position": "fixed",
-    "height": "4px",
-    "background": "linear-gradient(90deg, rgba(30,69,171,1) 0%, rgba(81,18,135,1) 34%, rgba(142,19,96,1) 69%, rgba(144,7,18,1) 100%)",
-   });
+  let eleStyle = progBarEle.style;
+  eleStyle.opacity = "1";
+  eleStyle.margin = "0px";
+  eleStyle.padding = "0px";
+  eleStyle.left = "0px";
+  eleStyle.width = `${progressBarWidth}`;
+  eleStyle.zIndex = "1000000001";
+  eleStyle.position = "fixed";
+  eleStyle.height = "4px";
+  eleStyle.background = "linear-gradient(90deg, rgba(30,69,171,1) 0%, rgba(81,18,135,1) 34%, rgba(142,19,96,1) 69%, rgba(144,7,18,1) 100%)";
+
+  document.body.insertBefore(progBarEle, document.body.childNodes[0]);
 }
 
 function updateFreeShippingBar(goalAmount, cartAmount) {
   const barMessage = getBarMessage(goalAmount, cartAmount);
-
-  if($('.free-shipping-bar').length) {
-    $('.free-shipping-bar .text-container').text(barMessage);
+  const freeShippingBarEle = document.querySelector('.free-shipping-bar');
+  if(freeShippingBarEle) {
+    freeShippingBarEle.firstChild.textContent = barMessage;
   } else {
     setupFreeShippingBar(barMessage);
   }
 }
 
 function setupFreeShippingBar(message) {
-   $('body').prepend(`<div class="free-shipping-bar-container"><div class="free-shipping-bar"><div class="text-container">${message}</div></div></div>`);
+  const barContainer = document.createElement('div');
+  const freeShippingBar = document.createElement('div');
+  const textContainer = document.createElement('div');
 
-   $('.free-shipping-bar-container').css({
-    "display": 'block',
-    "color": 'inherit',
-    "height": '44px'
-   });
+  const barContainerStyle = barContainer.style;
+  barContainerStyle.display = "block";
+  barContainerStyle.color = "inherit";
+  barContainerStyle.height = "44px";
 
-   $('.free-shipping-bar').css({
-    "opacity": "1",
-    "margin": "0px",
-    "padding": "0px",
-    "left": "0px",
-    "height": "auto",
-    "width": "100%",
-    "z-index": "100000001",
-    "position": "fixed",
-    "top": "0px",
-   });
+  const fsbStyle = freeShippingBar.style;
+  fsbStyle.opacity = "1";
+  fsbStyle.margin = "0px"
+  fsbStyle.padding = "0px";
+  fsbStyle.left = "0px";
+  fsbStyle.height = "auto";
+  fsbStyle.width = "100%";
+  fsbStyle.zIndex = "100000001";
+  fsbStyle.position = "fixed";
+  fsbStyle.top = "0px";
 
-   $('.free-shipping-bar .text-container').css({
-    "text-align": "center",
-    "margin": "0px",
-    "margin-bottom": "0px",
-    "padding": "12px 10px",
-    "left": "0px",
-    "height": "auto",
-    "width": "100%",
-    "box-sizing": "border-box",
-    "border": "medium none",
-    "background-color": `${bgColor}`,
-    "color": `${txtColor}`,
-    "font-size": "16px",
-    "line-height": "20px",
-    "font-family": "Helvetica",
-   });
+  const txtContainerStyle = textContainer.style;
+  txtContainerStyle.textAlign = "center";
+  txtContainerStyle.margin = "0px";
+  txtContainerStyle.marginBottom = "0px";
+  txtContainerStyle.padding = "12px 10px";
+  txtContainerStyle.left = "0px";
+  txtContainerStyle.height = "auto";
+  txtContainerStyle.width = "100%";
+  txtContainerStyle.boxSizing = "border-box";
+  txtContainerStyle.border = "medium none";
+  txtContainerStyle.backgroundColor = `${bgColor}`;
+  txtContainerStyle.color = `${txtColor}`;
+  txtContainerStyle.fontSize = "16px";
+  txtContainerStyle.lineHeight = "20px";
+  txtContainerStyle.fontFamily = "Helvetica";
 
-   $(".sticky").css("top", "44px");
+  textContainer.textContent = message;
+  freeShippingBar.appendChild(textContainer);
+  barContainer.appendChild(freeShippingBar);
+
+  document.body.insertBefore(barContainer, document.body.childNodes[0]);
+
+  // $('body').prepend(`<div class="free-shipping-bar-container"><div class="free-shipping-bar"><div class="text-container">${message}</div></div></div>`);
+
+  //  $('.free-shipping-bar-container').css({
+  //   "display": 'block',
+  //   "color": 'inherit',
+  //   "height": '44px'
+  //  });
+
+  //  $('.free-shipping-bar').css({
+  //   "opacity": "1",
+  //   "margin": "0px",
+  //   "padding": "0px",
+  //   "left": "0px",
+  //   "height": "auto",
+  //   "width": "100%",
+  //   "z-index": "100000001",
+  //   "position": "fixed",
+  //   "top": "0px",
+  //  });
+
+  //  $('.free-shipping-bar .text-container').css({
+  //   "text-align": "center",
+  //   "margin": "0px",
+  //   "margin-bottom": "0px",
+  //   "padding": "12px 10px",
+  //   "left": "0px",
+  //   "height": "auto",
+  //   "width": "100%",
+  //   "box-sizing": "border-box",
+  //   "border": "medium none",
+  //   "background-color": `${bgColor}`,
+  //   "color": `${txtColor}`,
+  //   "font-size": "16px",
+  //   "line-height": "20px",
+  //   "font-family": "Helvetica",
+  //  });
+
+  document.querySelector('.sticky').style.top = "44px";
+  //$(".sticky").css("top", "44px");
 }
 
 async function getTotalCartValue() {
