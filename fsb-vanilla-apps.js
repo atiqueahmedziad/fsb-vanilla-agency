6f5170328c1aa192e3cb42e179acb6867c34dfee
Atique Ahmed Ziad <softfilebd@gmail.com>
2021-01-06 16:36:34 +0600

    Update fsb-vanilla-apps.js

--- fsb-vanilla-apps.js
+++ fsb-vanilla-apps.js
@@ -1,42 +1,52 @@
 let goalAmount = 0;
-let bgColor = "";
-let txtColor = "";
-let initialMsgBefore = "";
-let initialMsgAfter = "";
-let progressMsgBefore = "";
-let progressMsgAfter = "";
-let goalAchievedMsg = "";
-let progressBarWidth = "0";
+let bgColor = '';
+let txtColor = '';
+let initialMsgBefore = '';
+let initialMsgAfter = '';
+let progressMsgBefore = '';
+let progressMsgAfter = '';
+let goalAchievedMsg = '';
+let progressBarWidth = '0';
 let globalSettings = {};
+let currentCurrency = '';
+
+const currencyList = {
+  GBP: '$',
+  USD: '£',
+};
 
 function valWithDecimal(val) {
   const valStr = val.toString();
-  return valStr.slice(0, valStr.length-2) + "." + valStr.slice(valStr.length-2);
+  return (
+    valStr.slice(0, valStr.length - 2) + '.' + valStr.slice(valStr.length - 2)
+  );
 }
 
-function getBarMessage(goalAmount, cartAmount) {
-  goalAmount = parseFloat(goalAmount);
+function getBarMessage(goalAmnt, cartAmount) {
+  goalAmnt = parseFloat(goalAmnt);
   cartAmount = parseFloat(cartAmount);
 
-  if(cartAmount === 0) {
-    return `${initialMsgBefore} $${goalAmount} ${initialMsgAfter}`;
-  } else if(goalAmount > cartAmount) {
-    return `${progressMsgBefore} $${goalAmount-cartAmount} ${progressMsgAfter}`;
-  } else if(goalAmount <= cartAmount) {
+  if (cartAmount === 0) {
+    return `${initialMsgBefore} ${currentCurrency}${goalAmnt} ${initialMsgAfter}`;
+  } else if (goalAmnt > cartAmount) {
+    return `${progressMsgBefore} ${currentCurrency}${
+      goalAmnt - cartAmount
+    } ${progressMsgAfter}`;
+  } else if (goalAmnt <= cartAmount) {
     return `${goalAchievedMsg}`;
   }
 }
 
-function updateProgressBar(goalAmount, cartAmount) {
-  if(!globalSettings.pgBarEnabled) {
+function updateProgressBar(goalAmnt, cartAmount) {
+  if (!globalSettings.pgBarEnabled) {
     return;
   }
 
-  const newBarWidth = cartAmount / goalAmount * 100;
+  const newBarWidth = (cartAmount / goalAmnt) * 100;
   progressBarWidth = `${newBarWidth > 100 ? 100 : newBarWidth}`;
   const progressBarEle = document.querySelector('.fsb-progress-bar');
 
-  if(progressBarEle) {
+  if (progressBarEle) {
     progressBarEle.style.width = `${progressBarWidth}%`;
   } else {
     setupProgressBar(progressBarWidth);
@@ -48,27 +58,28 @@ function setupProgressBar(progressBarWidth) {
   progBarEle.classList.add('fsb-progress-bar');
 
   let eleStyle = progBarEle.style;
-  eleStyle.opacity = "1";
-  eleStyle.margin = "0px";
-  eleStyle.padding = "0px";
-  eleStyle.left = "0px";
+  eleStyle.opacity = '1';
+  eleStyle.margin = '0px';
+  eleStyle.padding = '0px';
+  eleStyle.left = '0px';
   eleStyle.width = `${progressBarWidth}%`;
-  eleStyle.zIndex = "1000000001";
-  eleStyle.position = "fixed";
-  eleStyle.height = "4px";
-  eleStyle.background = "linear-gradient(90deg, rgba(30,69,171,1) 0%, rgba(81,18,135,1) 34%, rgba(142,19,96,1) 69%, rgba(144,7,18,1) 100%)";
+  eleStyle.zIndex = '1000000001';
+  eleStyle.position = 'fixed';
+  eleStyle.height = '4px';
+  eleStyle.background =
+    'linear-gradient(90deg, rgba(30,69,171,1) 0%, rgba(81,18,135,1) 34%, rgba(142,19,96,1) 69%, rgba(144,7,18,1) 100%)';
 
   document.body.insertBefore(progBarEle, document.body.childNodes[0]);
 }
 
-function updateFreeShippingBar(goalAmount, cartAmount) {
-  if(!globalSettings.shpBarEnabled) {
+function updateFreeShippingBar(goalAmnt, cartAmount) {
+  if (!globalSettings.shpBarEnabled) {
     return;
   }
 
-  const barMessage = getBarMessage(goalAmount, cartAmount);
+  const barMessage = getBarMessage(goalAmnt, cartAmount);
   const freeShippingBarEle = document.querySelector('.free-shipping-bar');
-  if(freeShippingBarEle) {
+  if (freeShippingBarEle) {
     freeShippingBarEle.firstChild.textContent = barMessage;
   } else {
     setupFreeShippingBar(barMessage);
@@ -83,36 +94,36 @@ function setupFreeShippingBar(message) {
   freeShippingBar.classList.add('free-shipping-bar');
 
   const barContainerStyle = barContainer.style;
-  barContainerStyle.display = "block";
-  barContainerStyle.color = "inherit";
-  barContainerStyle.height = "44px";
+  barContainerStyle.display = 'block';
+  barContainerStyle.color = 'inherit';
+  barContainerStyle.height = '44px';
 
   const fsbStyle = freeShippingBar.style;
-  fsbStyle.opacity = "1";
-  fsbStyle.margin = "0px"
-  fsbStyle.padding = "0px";
-  fsbStyle.left = "0px";
-  fsbStyle.height = "auto";
-  fsbStyle.width = "100%";
-  fsbStyle.zIndex = "100000001";
-  fsbStyle.position = "fixed";
-  fsbStyle.top = "0px";
+  fsbStyle.opacity = '1';
+  fsbStyle.margin = '0px';
+  fsbStyle.padding = '0px';
+  fsbStyle.left = '0px';
+  fsbStyle.height = 'auto';
+  fsbStyle.width = '100%';
+  fsbStyle.zIndex = '100000001';
+  fsbStyle.position = 'fixed';
+  fsbStyle.top = '0px';
 
   const txtContainerStyle = textContainer.style;
-  txtContainerStyle.textAlign = "center";
-  txtContainerStyle.margin = "0px";
-  txtContainerStyle.marginBottom = "0px";
-  txtContainerStyle.padding = "12px 10px";
-  txtContainerStyle.left = "0px";
-  txtContainerStyle.height = "auto";
-  txtContainerStyle.width = "100%";
-  txtContainerStyle.boxSizing = "border-box";
-  txtContainerStyle.border = "medium none";
+  txtContainerStyle.textAlign = 'center';
+  txtContainerStyle.margin = '0px';
+  txtContainerStyle.marginBottom = '0px';
+  txtContainerStyle.padding = '12px 10px';
+  txtContainerStyle.left = '0px';
+  txtContainerStyle.height = 'auto';
+  txtContainerStyle.width = '100%';
+  txtContainerStyle.boxSizing = 'border-box';
+  txtContainerStyle.border = 'medium none';
   txtContainerStyle.backgroundColor = `${bgColor}`;
   txtContainerStyle.color = `${txtColor}`;
-  txtContainerStyle.fontSize = "16px";
-  txtContainerStyle.lineHeight = "20px";
-  txtContainerStyle.fontFamily = "Helvetica";
+  txtContainerStyle.fontSize = '16px';
+  txtContainerStyle.lineHeight = '20px';
+  txtContainerStyle.fontFamily = 'Helvetica';
 
   textContainer.textContent = message;
   freeShippingBar.appendChild(textContainer);
@@ -120,48 +131,53 @@ function setupFreeShippingBar(message) {
 
   document.body.insertBefore(barContainer, document.body.childNodes[0]);
 
-  document.querySelector('.sticky').style.top = "44px";
+  document.querySelector('.sticky').style.top = '44px';
 }
 
 async function getTotalCartValue() {
-   const cartResponse = await fetch(`${window.location.origin}/cart.js`);
-   if (cartResponse.ok) {
-      const cartObj = await cartResponse.json();
-      const totalCartValue = valWithDecimal(cartObj.original_total_price);
-      return totalCartValue;
-   } else {
-      console.error(cartResponse.status);
-   }
+  const cartResponse = await fetch(`${window.location.origin}/cart.js`);
+  if (cartResponse.ok) {
+    // set current currency symbol
+    currentCurrency = currencyList[cartResponse.currency];
+    const cartObj = await cartResponse.json();
+    const totalCartValue = valWithDecimal(cartObj.original_total_price);
+    return totalCartValue;
+  } else {
+    console.error(cartResponse.status);
+  }
 }
 
 const init = async () => {
-  const dataBody = await fetch('https://fsb.vanilla-apps.com/api/getgoalamount', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      "shopId": `${window.location.host}`,
-    }),
-  });
+  const dataBody = await fetch(
+    'https://fsb.vanilla-apps.com/api/getgoalamount',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        shopId: `${window.location.host}`,
+      }),
+    }
+  );
 
   const data = await dataBody.json();
   const { messages, styles, settings } = data;
 
   globalSettings = settings;
 
-  if(!settings.shpBarEnabled && !settings.pgBarEnabled) {
+  if (!settings.shpBarEnabled && !settings.pgBarEnabled) {
     return;
   }
 
   const totalCartAmount = await getTotalCartValue();
   goalAmount = data.goalAmount;
 
-  if(settings.pgBarEnabled) {
-    updateProgressBar(goalAmount, totalCartAmount)
+  if (settings.pgBarEnabled) {
+    updateProgressBar(goalAmount, totalCartAmount);
   }
 
-  if(settings.shpBarEnabled) {
+  if (settings.shpBarEnabled) {
     initialMsgBefore = messages.initialMsgBefore;
     initialMsgAfter = messages.initialMsgAfter;
     progressMsgBefore = messages.progressMsgBefore;
@@ -172,18 +188,18 @@ const init = async () => {
 
     updateFreeShippingBar(goalAmount, totalCartAmount);
   }
-}
+};
 
 const open = window.XMLHttpRequest.prototype.open;
 
 function openReplacement() {
-  this.addEventListener("load", function () {
+  this.addEventListener('load', function () {
     if (
       [
-      "/cart/add.js",
-      "/cart/update.js",
-      "/cart/change.js",
-      "/cart/clear.js",
+        '/cart/add.js',
+        '/cart/update.js',
+        '/cart/change.js',
+        '/cart/clear.js',
       ].includes(this._url)
     ) {
       calculateShipping(this.response);
@@ -196,9 +212,9 @@ function openReplacement() {
 window.XMLHttpRequest.prototype.open = openReplacement;
 
 async function calculateShipping(cartJson) {
-   const currCartAmount = await getTotalCartValue();
-   updateFreeShippingBar(goalAmount, currCartAmount);
-   updateProgressBar(goalAmount, currCartAmount);
+  const currCartAmount = await getTotalCartValue();
+  updateFreeShippingBar(goalAmount, currCartAmount);
+  updateProgressBar(goalAmount, currCartAmount);
 }
 
 init();
